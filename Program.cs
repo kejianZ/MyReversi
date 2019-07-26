075ec70581176fc35bdfe34acf371bf37a150b95
Kejian Zhu <kejianzhu@geotab.com>
2019-07-26 14:39:49 -0400

    start trace by git

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace chessTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
